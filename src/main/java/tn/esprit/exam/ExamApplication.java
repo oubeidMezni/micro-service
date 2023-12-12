d0aa207b2a2617ee75b2d37530c1bf2b0d302ad3
anas123x <anas.dai@esprit.tn>
2023-12-12 17:25:12 +0100

    eureka

--- src/main/java/tn/esprit/exam/ExamApplication.java
+++ src/main/java/tn/esprit/exam/ExamApplication.java
@@ -3,11 +3,13 @@ package tn.esprit.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 //@EnableAspectJAutoProxy
 public class ExamApplication {
 
