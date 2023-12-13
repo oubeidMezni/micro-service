5e8e40ea870888997308f57f90e3a4652a189254
RayenCW <mohamedrayen.dhraief@esprit.tn>
2023-12-13 14:50:58 +0100

    Forum

--- src/main/java/tn/esprit/exam/repository/TransportationRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.exam.repository;
-
-import tn.esprit.exam.entities.Transportation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportationRepository extends JpaRepository<Transportation, Long> {
-    // Add custom query methods if needed
-}
